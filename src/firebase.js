d5f76ae4c1b13783ddacde3c5e25083589906c25
Felix Armstrong Ezeudo <armstrongfelix4@gmail.com>
2026-08-12 18:59:59 +0100

    added  the envs

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhdcSKljsI5rmiz6hV_O-oRAg0KtejI2c",
-  authDomain: "sms-nis.firebaseapp.com",
-  projectId: "sms-nis",
-  storageBucket: "sms-nis.firebasestorage.app",
-  messagingSenderId: "765870434973",
-  appId: "1:765870434973:web:8019c7a305c4c42c5c0274",
-  measurementId: "G-ZMBX2NH0XQ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
